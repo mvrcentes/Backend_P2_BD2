7c599e8c98e12c267b7f0c9fcf8345cfb0554dc6
Cristales <mariocardona0501@gmail.com>
2024-04-17 17:17:15 -0600

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from py2neo import Graph
 
 #Database Maco
-graph = Graph("neo4j+s://60c47d8c.databases.neo4j.io", auth=("neo4j", "u6JMgCrz_YRDuU1GLMiQtZiDrlBlnqi7BEDtQydhr4E"))
+# graph = Graph("neo4j+s://60c47d8c.databases.neo4j.io", auth=("neo4j", "u6JMgCrz_YRDuU1GLMiQtZiDrlBlnqi7BEDtQydhr4E"))
 
 #database Javier
-#graph = Graph("neo4j+s://e2de6d39.databases.neo4j.io", auth=("neo4j", "Uf7eQq7CizX4Sy4mhwPpPgRTZ5wSLSqiiUV_LkcYr0Q"))
+graph = Graph("neo4j+s://e2de6d39.databases.neo4j.io", auth=("neo4j", "Uf7eQq7CizX4Sy4mhwPpPgRTZ5wSLSqiiUV_LkcYr0Q"))
